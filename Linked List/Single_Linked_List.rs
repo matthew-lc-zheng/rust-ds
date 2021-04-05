8503ef90bc3d21c3fc1c2dd73cc0f02520d4922d
Matthew <64306058+matthew-lc-zheng@users.noreply.github.com>
2021-04-05 15:02:22 +0800

    Update Single_Linked_List.rs

--- Linked List/Single_Linked_List.rs
+++ Linked List/Single_Linked_List.rs
@@ -1,73 +1,71 @@
-use std::fmt::Display;
+// use std::fmt::Display;
 
+use i32 as T;
 /// *main for test* ///
 
-// fn main() {
-//     let mut head = _list_from(vec![1, 2, 3, 4, 5]);
-//     _list_traverse(&head);
-//     head = _list_reverse(head);
-// 
-//     _list_traverse(&head);
-// }
-
-fn _list_from<T>(mut arr: Vec<T>) -> Option<Box<Node<T>>> {
-    arr.reverse();
-    let mut head: Option<Box<Node<T>>> = None;
-    for i in arr {
-        let mut node: Box<Node<T>> = Box::from(Node::new(i));
-        node.next = head;
-        head = Some(node);
-    }
-    head
+fn main() {
+    let mut head = make_list(&[1, 2, 3, 4, 5]);
+    traverse(head.as_ref());
+    println!("{}",length(head.as_ref()));
+    head=reverse(head);
+    traverse(head.as_ref());
 }
 
-fn _list_traverse<T: Display>(head: &Option<Box<Node<T>>>) {
+
+
+fn make_list(arr: &[T]) -> Option<Box<Node>> {
+    if arr.len()==0{
+        return None;
+    }
+    Some(Box::new(Node{
+        val: arr[0],
+        next:make_list(&arr[1..])
+    }))
+}
+
+fn traverse(head: Option<&Box<Node>>) {
     let mut cnt = 0;
-    let mut head_ = head.clone();
-    while let Some(node) = head_ {        
-        if node.next.is_none() { print!("{}", &node.val); } else { print!("{} -> ", &node.val); }
-        cnt += 1;
-        head_ = &node.next;
+    let mut node = head;
+    while node.is_some(){
+        cnt+=1;
+        if node.unwrap().next.is_none() {
+            print!("{}", &node.unwrap().val);
+        } else {
+            print!("{} -> ", &node.unwrap().val);
+        }
+        node=node.unwrap().next.as_ref();
     }
     println!("\nlength is {}", cnt);
 }
 
-fn _list_length<T>(head: &Option<Box<Node<T>>>) -> i32 {
+fn length(head: Option<&Box<Node>>) -> i32 {
     let mut cnt = 0;
-    let mut head_ = head.clone();
-    while let Some(node) = head_ {
-        cnt += 1;
-        head_ = &node.next;
+    let mut node = head;
+    while node.is_some(){
+        cnt+=1;
+        node=node.unwrap().next.as_ref();
     }
     cnt
 }
 
-fn _list_delete(head: &mut Option<Box<Node<T>>>,val:i32){
-    
-}
-
-fn _list_delete_all(head: &mut Option<Box<Node<T>>>,val:i32){
-    
-}
-
-fn _list_reverse<T>(mut head: Option<Box<Node<T>>>) -> Option<Box<Node<T>>> {    
-    let mut pre_head: Option<Box<Node<T>>> = None;
-    while let Some(mut node) = head.take() {
-        head = node.next.take();
-        node.next = pre_head;
-        pre_head = Some(node);
+fn reverse(mut head: Option<Box<Node>>)->Option<Box<Node>> {
+    let mut pre_head: Option<Box<Node>> = None;
+    while let Some(mut node)=head.take() {
+        head=node.next.take();
+        node.next=pre_head.take();
+        pre_head=Some(node);
     }
     pre_head
 }
 
 
 // Define node
-struct Node<T> {
+struct Node {
     val: T,
-    next: Option<Box<Node<T>>>,
+    next: Option<Box<Node>>,
 }
 
-impl<T> Node<T> {
+impl Node {
     fn new(val: T) -> Self {
         Node {
             next: None,
@@ -75,7 +73,3 @@ impl<T> Node<T> {
         }
     }
 }
-
-
-
-
